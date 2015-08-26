53b480a87a0ab9f887aa2c1048af2a7da4a3769b
narscpp4 <kdaskalova@nars.bg>
2015-08-26 16:03:51 +0300

    Java Team Practical Project

---
+++ TestingJoblessDB/src/teams/accounting_module/OwnerEJB.java
@@ -0,0 +1,25 @@
+package teams.accounting_module;
+
+import javax.ejb.Singleton;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import model.entity.Owner;
+
+
+@Singleton
+@SessionScoped
+public class OwnerEJB {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	//get owner information
+	public Owner getOwnerInformation(int id){
+		Owner owner = entityManager.find(Owner.class, id);
+		return owner;
+	}
+	
+
+}
---
+++ TestingJoblessDB/src/teams/admin_module/view/TestAdminModule.java
@@ -0,0 +1,9 @@
+package teams.admin_module.view;
+
+public class TestAdminModule {
+
+	public static void main(String[] args) {
+
+	}
+
+}
